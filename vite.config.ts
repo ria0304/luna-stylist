83aa2fdc187de19c9e3dbf549ee8ff1643021711
ria s <0304RIAS@GMAIL.COM>
2026-06-15 19:26:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,22 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
     },
   },
 });
